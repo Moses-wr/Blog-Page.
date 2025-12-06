b893452666d0981a36369ce9e4f3718f4479787f
Moses-wr <alexanderwagwara@gmail.com>
2025-12-06 10:29:10 +0300

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -26,7 +26,7 @@ const About = () => {
         <article>
           <h1 className="text-4xl font-extrabold">Hi, I’m your guide to Budget briefs and tax policy talks</h1>
           <p className="mt-4 text-muted-foreground">
-          Kenyan-based writer and researcher focused on analyzing recent government legislative changes, Treasury policies, and economic proposals.
+          Tax consultant and researcher focused on analyzing recent government legislative changes, Treasury policies, and economic proposals.
              Our work blends in-depth coverage of Kenya’s fiscal landscape with insights into international affairs, offering readers clear, timely, and well-researched perspectives on issues shaping both local and global. Feel free to reach us for any enquiries
           </p>
           <p className="mt-3 text-muted-foreground">
